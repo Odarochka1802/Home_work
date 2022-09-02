17d3540c9cb9bfb6f24393ab70dd3909d212d670
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2022-09-02 12:17:10 +0300

    Tadk_1

---
+++ Task_1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+ 
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+ 
+            if(a > b)
+            {
+                Console.WriteLine($"Первое число {a} больше второго {b}");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine($"Первое число {a} меньше второго {b}");
+            }
+            else
+            {
+                Console.WriteLine("Оба числа равны");
+            }
+
+        }
+    }
+}
