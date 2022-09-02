352c918e59ff1db03889a2fde8974d6355b1f7a1
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2022-09-02 12:17:53 +0300

    Task_2

---
+++ Task_2.cs
@@ -0,0 +1,20 @@
+﻿int Max(int a, int b, int c)
+{
+    int result = a;
+    if(b > result) result = b;
+    if(c > result) result = c;
+    return result;
+}
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+int max = Max(a,b,c);
+     
+
+Console.WriteLine($"Максимальное число: {max}");
